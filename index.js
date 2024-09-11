1a70ccb9f949ca7b11412851af5f0b51ff2e9038
shilpagollapudi999 <shilpagollapudi999@gmail.com>
2024-09-11 11:00:06 +0530

    shilpa

---
+++ index.js
@@ -0,0 +1,32 @@
+// let a="shery"
+// let b=a;
+// b="shilpa"
+// console.log(a)
+// console.log(b)
+// let parent =document.getElementById("parent");
+// let btn =document.getElementById("btn");
+// let toggle=1
+
+// btn.addEventListener("click",function(){
+//     if(toggle==1){
+//     parent.style.backgroundColor="rgb(52,52,52)"
+//     toggle=0
+//     }else{
+//         parent.style.backgroundColor="beige"
+//         toggle=1
+ 
+//     }
+// document.getElementById("parent").addEventListener("click",=>)
+
+// })
+
+
+// let a=123.456;
+// console.log(Math.ceil(a)) 
+
+// console.log(Math.random()*10)
+// console.log(Math.floor(Math.random()*10)) if u get 0 then use this +1
+// console.log(Math.floor(Math.random()*10)+1) then u will not get 0
+// let max=55;
+// let min=40;
+// console.log(Math.floor((Math.random()*(max-min+1))+min))
---
+++ mern.js
@@ -0,0 +1,8 @@
+function greet(params){
+    console.log("function greet")
+}
+function sum(a,b){
+    return a+b;
+}
+greet();
+let result=sum(1,2);
